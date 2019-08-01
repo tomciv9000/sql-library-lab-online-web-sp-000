f00e6a5831a9ff91307ae5f69d70da8b5a9669b4
Tom White <eugene.white@gmail.com>
2019-08-01 04:47:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,11 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) as total
+FROM characters
+GROUP BY species
+ORDER BY total DESC
+LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
